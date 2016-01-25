1fac293db512bb0a44dc02a3af094b232575c24b
Petar Ivanov <pepincho95@gmail.com>
2016-01-25 20:51:52 +0200

    Add an improved solution of task 2.

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,37 +1,49 @@
-def get_position_ahead(snake_head, direction)
-  snake_head.map.with_index { |m, i| m + direction[i] }
-end
-
 def move(snake, direction)
-  snake.drop(1) << get_position_ahead(snake[-1], direction)
+  grow(snake, direction).drop(1)
 end
 
 def grow(snake, direction)
-  snake.drop(0) << get_position_ahead(snake[-1], direction)
+  snake + [position_in_front_of_snake(snake_head(snake), direction)]
 end
 
 def new_food(food, snake, dimensions)
-  taken_fields = food + snake
-  widths = (0..dimensions[:width] - 1).to_a
-  heights = (0..dimensions[:height] - 1).to_a
-  all_fields = widths.product(heights)
-  all_fields.select { |v| !taken_fields.include?(v) }.sample
-end
+  xs, ys = (0...dimensions[:width]).to_a, (0...dimensions[:height]).to_a
+  all_positions = xs.product(ys)
+  empty_positions = all_positions - (food + snake)
 
-def position_in_bounds?(new_snake, dimensions)
-  ((new_snake.first >= dimensions[:width] or new_snake.first < 0) or
-  (new_snake.last >= dimensions[:height] or new_snake.last < 0)) ? true : false
+  empty_positions.sample
 end
 
 def obstacle_ahead?(snake, direction, dimensions)
-  new_position = get_position_ahead(snake[-1], direction)
-  (snake.include?(new_position) or
-  position_in_bounds?(new_position, dimensions)) ? true : false
+  next_position = position_in_front_of_snake(snake_head(snake), direction)
+  not in_bounds?(next_position, dimensions) or
+    body_ahead?(next_position, snake)
 end
 
 def danger?(snake, direction, dimensions)
-  new_position_1 = get_position_ahead(snake[-1], direction)
-  new_position_2 = get_position_ahead(new_position_1, direction)
-  (position_in_bounds?(new_position_1, dimensions) or
-   position_in_bounds?(new_position_2, dimensions)) ? true : false
+  obstacle_ahead?(snake, direction, dimensions) or
+    obstacle_ahead?(move(snake, direction), direction, dimensions)
+end
+
+def snake_head(snake)
+  snake.last
+end
+
+def position_in_front_of_snake(head, direction)
+  head_x, head_y = head
+  direction_x, direction_y = direction
+
+  [head_x + direction_x, head_y + direction_y]
+end
+
+def in_bounds?(position, dimensions)
+  x, y = position
+  width = dimensions[:width]
+  height = dimensions[:height]
+
+  x.between?(0, width - 1) and y.between?(0, height - 1)
+end
+
+def body_ahead?(position, snake)
+  snake.include?(position)
 end
